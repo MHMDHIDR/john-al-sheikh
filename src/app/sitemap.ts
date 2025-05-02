fd3facb9eed916ad299cae9b75ad206ff06d706f
Mohammed Haydar <mr.hamood277@gmail.com>
2025-05-02 11:16:58 +0100

    fixed sitemap routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,17 +4,17 @@ export const baseUrl = env.NEXT_PUBLIC_APP_URL;
 
 export default async function sitemap() {
   const routes = [
-    "/onboarding",
-    "/about",
-    "/contact",
-    "/privacy",
-    "/terms",
+    "onboarding",
+    "about",
+    "contact",
+    "privacy",
+    "terms",
     "mock-test",
     "buy-credits",
     "subscribe",
     "signin",
   ].map(route => ({
-    url: `${baseUrl}${route}`,
+    url: `${baseUrl}/${route}`,
   }));
 
   return [...routes];
