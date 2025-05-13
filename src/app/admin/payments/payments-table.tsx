9224cde52cd16afd300e4ff4ddf5b08e6e690593
Mohammed Haydar <mr.hamood277@gmail.com>
2025-05-13 10:16:58 +0100

    Added TestUsers Table

--- src/app/admin/payments/payments-table.tsx
+++ src/app/admin/payments/payments-table.tsx
@@ -3,19 +3,18 @@
 import { DataTable } from "@/components/custom/data-table";
 import { paymentsColumns } from "./payments-columns";
 import type { BaseEntity } from "@/components/custom/data-table/base-columns";
-import type { ColumnDef } from "@tanstack/react-table";
 
 export interface Payment extends BaseEntity {
   amount: number;
   currency: string;
   source_types: { card: number | undefined };
-  type: "available" | "pending" | string;
+  type: string;
 }
 
 export default function PaymentsTable({ payments }: { payments: Payment[] }) {
   return (
     <DataTable<Payment>
-      columns={paymentsColumns as ColumnDef<Payment>[]}
+      columns={paymentsColumns}
       data={payments}
       emptyStateMessage="No payments found."
     />
