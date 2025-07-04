b5a946d31b2ec0a343ca6f0a8bc0212fd8dcc7b3
Mohammed Haydar <mr.hamood277@gmail.com>
2025-07-04 17:50:58 +0100

    added a nice character & logo for the homepage options

--- src/components/custom/homepage-options.tsx
+++ src/components/custom/homepage-options.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Image from "next/image";
 import { HomePageInteractiveGridPattern } from "@/components/custom/quick-speaking-test";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
@@ -20,45 +21,61 @@ export default async function HomepagePagesOptions() {
     >
       <HomePageInteractiveGridPattern />
       <Card
-        className="z-10 w-full border m-4 md:mt-40 bg-gradient-to-br from-blue-50 to-blue-100 transition-all dark:from-blue-900 dark:to-blue-800 hover:from-blue-100 hover:to-blue-200 hover:dark:from-blue-800 hover:dark:to-blue-700"
-        href={isEnoughCredits ? "/mock-test" : "/buy-credits"}
-        asLink
-      >
-        <CardHeader className="relative pb-2">
-          <CardTitle className="text-xl font-bold text-center">
-            جرِب المحادثة مع مدرب الـ IELTS
-          </CardTitle>
-          <CardDescription className="text-sm text-blue-700 dark:text-blue-300">
-            قم بإختبار نفسك في مهارة المحادثة وتعلم المزيد عن الـ IELTS
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="relative flex h-40 items-center justify-center">
-          <Button
-            className="pointer-events-none text-center text-xl font-medium text-blue-800 dark:text-blue-200"
-            variant={"outline"}
-          >
-            إختبر نفسك
-          </Button>
-        </CardContent>
-      </Card>
-
-      <Card
-        className="z-10 w-full border m-4 md:mt-40 bg-gradient-to-br from-purple-50 to-purple-100 transition-all dark:from-purple-900 dark:to-purple-800 hover:from-purple-100 hover:to-purple-200 hover:dark:from-purple-800 hover:dark:to-purple-700"
+        className="z-10 w-full border min-h-56 md:min-h-68 m-4 md:mt-40 bg-gradient-to-br from-purple-50 to-purple-100 transition-all dark:from-purple-900 dark:to-purple-800 hover:from-purple-100 hover:to-purple-200 hover:dark:from-purple-800 hover:dark:to-purple-700"
         href={isEnoughCredits ? "/general-english" : "/buy-credits"}
         asLink
       >
         <CardHeader className="relative pb-2">
+          <Image
+            src="/john-al-shiekh-character.png"
+            alt="English Speaking"
+            width={48}
+            height={48}
+            className="size-12 mx-auto"
+            loading="lazy"
+          />
           <CardTitle className="text-xl font-bold text-center">إتحدى نفسك!</CardTitle>
-          <CardDescription className="text-sm text-purple-700 dark:text-purple-300">
+          <CardDescription className="text-sm text-purple-700 dark:text-purple-300 text-center">
             قم بتجربة المحادثة العامة باللغة الإنجليزية مع مساعد لتحسين مهاراتك
           </CardDescription>
         </CardHeader>
-        <CardContent className="relative flex h-40 items-center justify-center">
+        <CardContent className="relative flex h-14 md:min-h-32 items-center justify-center">
           <Button
             className="pointer-events-none text-center text-xl font-medium text-purple-800 dark:text-purple-200"
             variant={"secondary"}
           >
             جرب الآن
+          </Button>
+        </CardContent>
+      </Card>
+
+      <Card
+        className="z-10 w-full border min-h-56 md:min-h-68 m-4 md:mt-40 bg-gradient-to-br from-blue-50 to-blue-100 transition-all dark:from-blue-900 dark:to-blue-800 hover:from-blue-100 hover:to-blue-200 hover:dark:from-blue-800 hover:dark:to-blue-700"
+        href={isEnoughCredits ? "/mock-test" : "/buy-credits"}
+        asLink
+      >
+        <CardHeader className="relative pb-2">
+          <CardTitle className="text-xl font-bold text-center">
+            <Image
+              src="/ielts-logo.png"
+              alt="IELTS Speaking"
+              width={60}
+              height={48}
+              className="w-fit mx-auto"
+              loading="lazy"
+            />
+            جرِب المحادثة مع مدرب الـ IELTS
+          </CardTitle>
+          <CardDescription className="text-sm text-blue-700 dark:text-blue-300 text-center">
+            قم بإختبار نفسك في مهارة المحادثة وتعلم المزيد عن الـ IELTS
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="relative flex h-14 md:min-h-32 items-center justify-center">
+          <Button
+            className="pointer-events-none text-center text-xl font-medium text-blue-800 dark:text-blue-200"
+            variant={"outline"}
+          >
+            إختبر نفسك
           </Button>
         </CardContent>
       </Card>
